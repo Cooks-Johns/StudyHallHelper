0d75163731867729dbad63a8e831fb4c797316da
Cooks-Johns <cooks.johns@gmail.com>
2020-11-11 17:58:44 -0600

    Please look at this code

--- gitHelper/gitHub_helper.js
+++ gitHelper/gitHub_helper.js
@@ -1,1 +1,14 @@
 // This is how we are testing 
+
+function showMoves() {
+    var i = 0;
+    var moves = setInterval(function(){
+        playGame(game.yourGame[i]);
+        i++;
+        if (i >= game.yourGame.length) {
+            clearInterval(moves);
+        }
+    }, 800);
+
+    reset();
+}
